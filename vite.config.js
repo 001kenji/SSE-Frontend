5a9dce87708637660629c498db54c81410d5b112
BrianNjuguna <142185778+001kenji@users.noreply.github.com>
2024-05-24 09:57:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from "vite";
 export default defineConfig({
-  build: { manifest: true },
+ build: {
+    manifest: true,
+    commonjsOptions: { transformMixedEsModules: true } // Change
+  },
   base: process.env.mode === "production" ? "/static/" : "/",
   root: "./",
   plugins: [react()],
